b872150f79fc9aed16e3d9dd661e4d0ba69bcdfa
Disha Talreja <dishatalreja1202@gmail.com>
2023-06-14 22:34:41 +0530

    fix: cors error

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   devServer:{
+//     proxy: "https://learn.myllama.co/webservice/rest/server.php",
+//   }
+// })
+module.exports = {
+  devServer: {
+    proxy: 'https://learn.myllama.co/'
+  }
+}
