55feff7b98929999799acff0556e85b101fb82b1
Disha Talreja <dishatalreja1202@gmail.com>
2023-06-16 14:44:38 +0530

    updated code

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 // const { defineConfig } = require('@vue/cli-service')
 // module.exports = defineConfig({
 //   transpileDependencies: true,
-//   devServer:{
-//     proxy: "https://learn.myllama.co/webservice/rest/server.php",
-//   }
 // })
 module.exports = {
   devServer: {
